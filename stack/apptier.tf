efcd03df030492ad270112813497acf8d129c225
mgurkan27 <mustafagurkan27@gmail.com>
2020-09-11 22:20:39 -0400

    new

--- stack/apptier.tf
+++ stack/apptier.tf
@@ -47,6 +47,12 @@ resource "aws_security_group" "group3_app_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
--- stack/webtier.tf
+++ stack/webtier.tf
@@ -53,6 +53,12 @@ resource "aws_security_group" "group3_web_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
