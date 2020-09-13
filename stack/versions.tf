88991e9de96e8964b2bc7f8937d362b5f87734ff
mgurkan27 <mustafagurkan27@gmail.com>
2020-09-13 15:51:20 -0400

    new

--- stack/versions.tf
+++ stack/versions.tf
@@ -6,6 +6,8 @@ terraform {
 terraform {
   required_providers {
     aws = {
+      region     = var.aws_region
+      profile    = var.aws_profile
       version = ">= 2.7.0"
       source  = "hashicorp/aws"
     }
